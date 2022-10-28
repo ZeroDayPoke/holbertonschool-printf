0e28826c9c7f51a94aa6ff3c73d006fd51b943d3
Chris Stamper <cstampok@gmail.com>
2022-10-27 23:27:28 -0500

    changed increment positioning

--- _printf.c
+++ _printf.c
@@ -9,35 +9,31 @@ int _printf(const char *format, ...)
 {
 	int i = 0, j, totesWrite = 0, hcf_check;
 	va_list varArg;
-	_pcall betty[] = {
-		{p_all_int, 'd'},
-		{p_all_int, 'i'},
-		{p_all_char, 'c'},
-		{p_all_str, 's'}
-	};
+	_pcall betty[] = {{p_all_int, 'd'}, {p_all_int, 'i'}, {p_all_char, 'c'}, {p_all_str, 's'}};
 
 	hcf_check = _hcf(format);
 	if (hcf_check == -1)
-	{
 		return (-1);
-	}
 	va_start(varArg, format);
 	while (format && format[i + 1])
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '%')
+			if (format[i + 1] == '%')
 			{
 				totesWrite += write_to_SO('%');
 			}
-			for (j = 0; betty[j].formChar; j++)
+			else
 			{
-				if (format[i] == betty[j].formChar)
+				for (j = 0; betty[j].formChar; j++)
 				{
-					totesWrite += betty[j].fun(varArg);
+					if (format[i + 1] == betty[j].formChar)
+					{
+						totesWrite += betty[j].fun(varArg);
+					}
 				}
 			}
+			i++;
 		}
 		else
 		{
