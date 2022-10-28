ea297f6febd94c60218739efeb9e805d0eb5d417
spindouken <spindouken@gmail.com>
2022-10-28 10:56:13 -0500

    NULL edge case attempt

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 	if (hcf_check == -1)
 		return (-1);
 	va_start(varArg, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
 	while (format && format[i])
 	{
 		if (format[i] == '%')
