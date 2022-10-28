cf600a3db858e03f2326292aee6e05e04c24526a
spindouken <spindouken@gmail.com>
2022-10-28 10:52:19 -0500

    betty compliance

--- _printf.c
+++ _printf.c
@@ -9,7 +9,8 @@ int _printf(const char *format, ...)
 {
 	int i = 0, j, totesWrite = 0, hcf_check;
 	va_list varArg;
-	_pcall betty[] = {{p_all_int, 'd'}, {p_all_int, 'i'}, {p_all_char, 'c'}, {p_all_str, 's'}};
+	_pcall betty[] = {{p_all_int, 'd'}, {p_all_int, 'i'},
+	{p_all_char, 'c'}, {p_all_str, 's'}};
 
 	hcf_check = _hcf(format);
 	if (hcf_check == -1)
